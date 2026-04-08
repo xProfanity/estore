98f0346c08dd2ebf1f6b81830d476f30077a0d5a
xprofanity <pecpalmplay@gmail.com>
2026-04-08 13:31:03 +0200

    implements postgres db client

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,4 +1,14 @@
+import {dbClient} from "@/lib/db"
+
 export async function GET(request: Request) {
 
-	return Response.json({message: "Getting categorie"})
+	try {
+		const client = await dbClient()
+		const result = await client.query('select id,name from product_category')	
+		
+		return Response.json({rows: result.rows, status: 200})
+	} catch(err) {
+		console.log(err)
+		return Response.json({status: 500})
+	}
 }
--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,23 @@
-// init db
+import Pool from "pg-pool";
+
+const pool = new Pool({
+		database: 'store',
+		user: 'postgres',
+		port: 5432,
+		ssl: process.env.NODE_ENV === "production",
+		max: 20, 
+		idleTimeoutMillis: 1000,
+		connectionTimeoutMillis: 1000, 
+		maxUses: 7500,
+})
+
+export async function dbClient() {
+	try {
+		const client = await pool.connect()
+		return client
+	} catch(err) {
+		console.error("query error")
+		throw err?.message;
+	}
+}
+
