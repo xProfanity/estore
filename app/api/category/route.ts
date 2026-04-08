9bc896561fe6e8c0c0b95d51e85031a61d713b28
xprofanity <pecpalmplay@gmail.com>
2026-04-08 15:36:00 +0200

    client release to the pool

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -5,6 +5,8 @@ export async function GET(request: Request) {
 	try {
 		const client = await dbClient()
 		const result = await client.query('select id,name from product_category')	
+
+		client.release()
 		
 		return Response.json({rows: result.rows}, {status: 200})
 	} catch(err) {
